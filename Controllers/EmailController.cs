c58aa3e5d6cf0145f8846987acdb7e5f636819ea
Zuhair Haroon Khan <zk222ac@gmail.com>
2021-02-28 22:47:54 +0100

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using EmailService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailService.Controllers
+{
+    public class EmailController : Controller
+    {
+        //Turn on the Access for less secure apps
+        public async Task<IActionResult> Index(Email email)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mMessage = new MailMessage();
+                mMessage.To.Add(email.To);
+                mMessage.Subject = email.Subject;
+                mMessage.Body = email.Body;
+                mMessage.IsBodyHtml = false;
+                mMessage.From = new MailAddress("zukh4321@gmail.com");
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                // smtp 
+                smtpClient.Port = 587;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new NetworkCredential("zukh4321@gmail.com", "Roskilde001!");
+                
+                await smtpClient.SendMailAsync(mMessage);
+                ViewData["Message"] = "Message has been send Succesfully";
+            }
+            return View();
+        }
+
+       
+    }
+   
+}
